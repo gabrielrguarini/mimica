797b22d79ee2c60d0f32f9a1914bb5a0fcd899e6
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-08-06 13:38:29 -0300

    refactor: move formatTime for utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatTime = (seconds: number) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes}:${
+    remainingSeconds < 10 ? `0${remainingSeconds}` : remainingSeconds
+  }`;
+};
