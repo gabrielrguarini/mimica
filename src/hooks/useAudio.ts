ad969a3710aa8816c9043e88afc46aeee26f5e4a
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-08-06 13:21:19 -0300

    feat: add useCallback in useAudio

--- src/hooks/useAudio.ts
+++ src/hooks/useAudio.ts
@@ -1,23 +1,23 @@
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
 
 export default function useAudio(url: string) {
   const audioRef = useRef<HTMLAudioElement | null>(null);
 
-  const playAudio = () => {
+  const playAudio = useCallback(() => {
     if (audioRef.current) {
       audioRef.current.currentTime = 0;
     } else {
       audioRef.current = new Audio(url);
     }
     audioRef.current.play();
-  };
+  }, [url]);
 
-  const stopAudio = () => {
+  const stopAudio = useCallback(() => {
     if (audioRef.current) {
       audioRef.current.pause();
       audioRef.current.currentTime = 0;
     }
-  };
+  }, []);
 
   return { playAudio, stopAudio };
 }
