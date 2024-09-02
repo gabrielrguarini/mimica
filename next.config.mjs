4c600557569a2cb82a4d95954c8ae87b2a56dc9c
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-09-02 14:26:31 -0300

    merge pwa

--- next.config.mjs
+++ next.config.mjs
@@ -2,20 +2,7 @@ import withPWA from "next-pwa";
 
 const nextConfig = withPWA({
   dest: "public",
-  disable: process.env.NODE_ENV === "development", // Desabilitar o PWA em modo de desenvolvimento
-  runtimeCaching: [
-    {
-      urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/i, // Cache de imagens
-      handler: "CacheFirst",
-      options: {
-        cacheName: "image-cache",
-        expiration: {
-          maxEntries: 20,
-          maxAgeSeconds: 24 * 60 * 60, // 1 dia
-        },
-      },
-    },
-  ],
+  disable: process.env.NODE_ENV === "development", // Desabilita o PWA no desenvolvimento
 });
 
 export default nextConfig;
